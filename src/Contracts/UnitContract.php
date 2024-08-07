30c234caab530aba12c1968d21ebe3b0c26264b3
majid mohammadian <majeedmohammadian@gmail.com>
2024-08-08 00:15:28 +0330

    add UnitContract file

---
+++ src/Contracts/UnitContract.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace JobMetric\Unit\Contracts;
+
+interface UnitContract
+{
+    /**
+     * unit allows the type.
+     *
+     * @return array
+     */
+    public function unitAllowTypes(): array;
+}
