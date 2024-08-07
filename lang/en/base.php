524336e960afa5775c3ee49c60c86a793c054b69
majid mohammadian <majeedmohammadian@gmail.com>
2024-08-08 00:14:46 +0330

    add language file

---
+++ lang/en/base.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Unit Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Unit for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    "validation" => [
+        "errors" => "Validation errors occurred.",
+    ],
+
+    "messages" => [
+    ],
+
+    "exceptions" => [
+        "model_unit_contract_not_found" => "Model ':model' not implements 'JobMetric\Unit\Contracts\UnitContract' interface!",
+    ],
+
+];
