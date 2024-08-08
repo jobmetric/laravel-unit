c245aa559d7a468c915fbaae43ea186da4a77230
majid mohammadian <majeedmohammadian@gmail.com>
2024-08-08 23:36:57 +0330

    change language file

--- lang/en/base.php
+++ lang/en/base.php
@@ -14,9 +14,20 @@ return [
 
     "validation" => [
         "errors" => "Validation errors occurred.",
+        "object_not_found" => "Unit not found!",
+        "unit_type_default_value_error" => "The desired :unit unit must have a default value of 1 in the first step and you cannot enter another value",
+        "unit_type_use_default_value_error" => "You have already entered the default value and you can no longer use the value 1",
+        "unit_type_cannot_change_default_value" => "You cannot change the default value",
     ],
 
     "messages" => [
+        "found" => "Unit found.",
+        "created" => "Unit created successfully.",
+        "updated" => "Unit updated successfully.",
+        "deleted" => "Unit deleted successfully.",
+        "restored" => "Unit restored successfully.",
+        "permanently_deleted" => "Unit permanently deleted successfully.",
+        "change_default_value" => "Default value changed successfully.",
     ],
 
     "exceptions" => [
