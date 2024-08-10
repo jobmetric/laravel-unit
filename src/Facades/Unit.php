27c3234fcf1601136309ce44a64691ee1bd2c0ea
majid mohammadian <majeedmohammadian@gmail.com>
2024-08-10 22:48:58 +0330

    add facade methods

--- src/Facades/Unit.php
+++ src/Facades/Unit.php
@@ -8,6 +8,13 @@ use Illuminate\Support\Facades\Facade;
  * @method static \Spatie\QueryBuilder\QueryBuilder query(array $filter = [], array $with = [], string $mode = null)
  * @method static \Illuminate\Http\Resources\Json\AnonymousResourceCollection paginate(array $filter = [], int $page_limit = 15, array $with = [], string $mode = null)
  * @method static \Illuminate\Http\Resources\Json\AnonymousResourceCollection all(array $filter = [], array $with = [], string $mode = null)
+ * @method static array get(int $unit_id, array $with = [], string $mode = null)
+ * @method static array store(array $data)
+ * @method static array update(int $unit_id, array $data)
+ * @method static array delete(int $unit_id)
+ * @method static array restore(int $unit_id)
+ * @method static array forceDelete(int $unit_id)
+ * @method static array changeDefaultValue(int $unit_id)
  *
  * @see \JobMetric\Unit\Unit
  */
