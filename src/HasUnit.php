b53ff11510f0b251b2a361ac426b37239d05e5f6
majid mohammadian <majeedmohammadian@gmail.com>
2024-08-11 19:44:43 +0330

    add attach and detach unit to any model

--- src/HasUnit.php
+++ src/HasUnit.php
@@ -4,6 +4,7 @@ namespace JobMetric\Unit;
 
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 use JobMetric\Unit\Exceptions\ModelUnitContractNotFoundException;
+use JobMetric\Unit\Http\Resources\UnitResource;
 use JobMetric\Unit\Models\Unit;
 use Throwable;
 
@@ -41,5 +42,111 @@ trait HasUnit
         return $this->morphToMany(Unit::class, 'unitable', config('unit.tables.unit_relation'))
             ->withPivot('type')
             ->withTimestamps(['created_at']);
+    }
+
+    /**
+     * attach unit
+     *
+     * @param int $unit_id
+     * @param string $type
+     * @param float $value
+     *
+     * @return array
+     * @throws Throwable
+     */
+    public function attachUnit(int $unit_id, string $type, float $value): array
+    {
+        /**
+         * @var Unit $unit
+         */
+        $unit = Unit::withTrashed()->find($unit_id);
+
+        if (!$unit) {
+            return [
+                'ok' => false,
+                'message' => trans('unit::base.validation.errors'),
+                'errors' => [
+                    'form' => [
+                        trans('unit::base.validation.object_not_found')
+                    ]
+                ],
+                'status' => 404
+            ];
+        }
+
+        $unitAllowTypes = $this->unitAllowTypes();
+
+        if (!in_array($type, $unitAllowTypes)) {
+            return [
+                'ok' => false,
+                'message' => trans('unit::base.validation.errors'),
+                'errors' => [
+                    'form' => [
+                        trans('unit::base.validation.unit_type_not_in_unit_allow_types')
+                    ]
+                ],
+                'status' => 422
+            ];
+        }
+
+        $this->units()->attach($unit_id, [
+            'type' => $type,
+            'value' => $value
+        ]);
+
+        return [
+            'ok' => true,
+            'message' => trans('unit::base.messages.attached'),
+            'data' => UnitResource::make($unit),
+            'status' => 200
+        ];
+    }
+
+    /**
+     * detach unit
+     *
+     * @param int $unit_id
+     *
+     * @return array
+     */
+    public function detachUnit(int $unit_id): array
+    {
+        foreach ($this->units as $unit) {
+            if ($unit->id == $unit_id) {
+                $data = UnitResource::make($unit);
+
+                $this->units()->detach($unit_id);
+
+                return [
+                    'ok' => true,
+                    'message' => trans('unit::base.messages.detached'),
+                    'data' => $data,
+                    'status' => 200
+                ];
+            }
+        }
+
+        return [
+            'ok' => false,
+            'message' => trans('unit::base.validation.errors'),
+            'errors' => [
+                'form' => [
+                    trans('unit::base.validation.object_not_found')
+                ]
+            ],
+            'status' => 404
+        ];
+    }
+
+    /**
+     * Get unit by type
+     *
+     * @param string $type
+     *
+     * @return MorphToMany
+     */
+    public function getMediaByType(string $type): MorphToMany
+    {
+        return $this->units()->wherePivot('type', $type);
     }
 }
