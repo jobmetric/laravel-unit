b2c01e936ca96d8466080521784311b13e8859df
majid mohammadian <majeedmohammadian@gmail.com>
2024-08-11 20:06:25 +0330

    add test HasUnit trait

--- tests/BaseUnit.php
+++ tests/BaseUnit.php
@@ -3,6 +3,9 @@
 namespace JobMetric\Unit\Tests;
 
 use App\Models\Product;
+use JobMetric\Unit\Enums\UnitTypeEnum;
+use JobMetric\Unit\Facades\Unit;
+use JobMetric\Unit\Models\Unit as UnitModels;
 use Tests\BaseDatabaseTestCase as BaseTestCase;
 
 class BaseUnit extends BaseTestCase
@@ -15,5 +18,27 @@ class BaseUnit extends BaseTestCase
     public function create_product(): Product
     {
         return Product::factory()->create();
+    }
+
+    /**
+     * create a fake unit
+     *
+     * @return UnitModels
+     */
+    public function create_unit(): UnitModels
+    {
+        Unit::store([
+            'type' => UnitTypeEnum::WEIGHT(),
+            'value' => 1,
+            'status' => true,
+            'translation' => [
+                'name' => 'Gram',
+                'code' => 'g',
+                'position' => 'left',
+                'description' => 'The gram is a metric system unit of mass.',
+            ],
+        ]);
+
+        return UnitModels::find(1);
     }
 }
--- tests/HasUnitTest.php
+++ tests/HasUnitTest.php
@@ -15,4 +15,59 @@ class HasUnitTest extends BaseUnit
         $product = new Product();
         $this->assertInstanceOf(\Illuminate\Database\Eloquent\Relations\MorphToMany::class, $product->units());
     }
+
+    /**
+     * @throws Throwable
+     */
+    public function test_attach(): void
+    {
+        $product = $this->create_product();
+
+        $unit = $this->create_unit();
+
+        $attach = $product->attachUnit($unit->id, 'weight', 1);
+
+        $this->assertIsArray($attach);
+
+        $this->assertDatabaseHas(config('unit.tables.unit_relation'), [
+            'unit_id' => $unit->id,
+            'unitable_id' => $product->id,
+            'unitable_type' => Product::class,
+            'type' => 'weight',
+            'value' => 1,
+        ]);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function test_detach(): void
+    {
+        $product = $this->create_product();
+
+        $unit = $this->create_unit();
+
+        $product->attachUnit($unit->id, 'weight', 1);
+
+        $detach = $product->detachUnit($unit->id);
+
+        $this->assertIsArray($detach);
+
+        $this->assertDatabaseMissing(config('unit.tables.unit_relation'), [
+            'unit_id' => $unit->id,
+            'unitable_id' => $product->id,
+            'unitable_type' => Product::class,
+            'type' => 'weight',
+            'value' => 1,
+        ]);
+    }
+
+    /**
+     * @throws Throwable
+     */
+    public function test_get_unit_by_type(): void
+    {
+        $product = new Product();
+        $this->assertInstanceOf(\Illuminate\Database\Eloquent\Relations\MorphToMany::class, $product->getMediaByType('weight'));
+    }
 }
