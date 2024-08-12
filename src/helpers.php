c98cc1c492428075829d59112fe760c957a0aeb8
majid mohammadian <majeedmohammadian@gmail.com>
2024-08-13 01:23:34 +0330

    add helper function convert

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,20 @@
 <?php
+
+use JobMetric\Unit\Facades\Unit;
+
+if (!function_exists('unitConvert')) {
+    /**
+     * unit convert
+     *
+     * @param int $from_unit_id
+     * @param int $to_unit_id
+     * @param float $value
+     *
+     * @return float
+     * @throws Throwable
+     */
+    function unitConvert(int $from_unit_id, int $to_unit_id, float $value): float
+    {
+        return Unit::convert($from_unit_id, $to_unit_id, $value);
+    }
+}
